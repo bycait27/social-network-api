d62288aa17d2fb77c2c87206f7a996a4ad785bf3
bycait27 <caitlinba27@gmail.com>
2025-06-21 20:02:45 -0500

    Fixed Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,41 +1,40 @@
-const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
 // schema to create Thought model
 const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minLength: 1,
-            maxLength: 280,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now(),
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        reactions: [reactionSchema],
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        id: false,
-    }
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  },
 );
 
 // create a virtual property 'reactionCount' that gets the length of the thought's reactions array field
-thoughtSchema
-    .virtual('reactionCount')
-    .get(function () {
-        return this.reactions.length;
-    });
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
 
-    // initialize Thought model
-    const Thought = model('thought', thoughtSchema);
+// initialize Thought model
+const Thought = model("thought", thoughtSchema);
 
-    module.exports = Thought;
+module.exports = Thought;
+
