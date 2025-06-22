cf800519a1459e6415a174278c06cfbf52cb3d13
bycait27 <caitlinba27@gmail.com>
2025-06-21 19:31:58 -0500

    Cleaned up connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
-const { connect, connection } = require('mongoose');
+// connection to MongoDB
+const { connect, connection } = require("mongoose");
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/studentsDB";
 
 connect(connectionString);
 
 module.exports = connection;
+
