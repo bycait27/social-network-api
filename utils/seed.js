b3e55ee92b2e36527e5a23f3cf090fd8f75ec52d
bycait27 <caitlinba27@gmail.com>
2025-06-21 19:30:21 -0500

    Cleaned up seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -1,20 +1,29 @@
-const connection = require('../config/connection');
-const { User, Thought } = require('../models');
-const { getRandomUsername, createRandomThoughts, getRandomEmail } = require('./data');
+// inlcude necessary modules
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+const {
+  getRandomUsername,
+  createRandomThoughts,
+  getRandomEmail,
+} = require("./data");
 
-connection.on('error', (err) => err);
+connection.on("error", (err) => err);
 
-connection.once('open', async () => {
-  console.log('connected');
-  // Delete the collections if they exist
-  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+connection.once("open", async () => {
+  console.log("connected");
+  // delete the collections if they exist
+  let thoughtCheck = await connection.db
+    .listCollections({ name: "thoughts" })
+    .toArray();
   if (thoughtCheck.length) {
-    await connection.dropCollection('thoughts');
+    await connection.dropCollection("thoughts");
   }
-  
-  let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+
+  let userCheck = await connection.db
+    .listCollections({ name: "users" })
+    .toArray();
   if (userCheck.length) {
-    await connection.dropCollection('users');
+    await connection.dropCollection("users");
   }
 
   const users = [];
@@ -36,6 +45,6 @@ connection.once('open', async () => {
   // loop through the saved thoughts, for each thought we need to generate a thought response and insert the thought responses
   console.table(users);
   console.table(thoughts);
-  console.info('Seeding complete! 🌱');
+  console.info("Seeding complete! 🌱");
   process.exit(0);
 });
