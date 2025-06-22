1284ab55bd96a79fa43c259a65fb486adf055acf
bycait27 <caitlinba27@gmail.com>
2025-06-21 19:35:09 -0500

    Cleaned up index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
-const router = require('express').Router();
-const thoughtRoutes = require('./thoughtRoutes');
-const userRoutes = require('./userRoutes');
+// api routes
+const router = require("express").Router();
+const thoughtRoutes = require("./thoughtRoutes");
+const userRoutes = require("./userRoutes");
 
-router.use('/thoughts', thoughtRoutes);
-router.use('/users', userRoutes);
+router.use("/thoughts", thoughtRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
+
